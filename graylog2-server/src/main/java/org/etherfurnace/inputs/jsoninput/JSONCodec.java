08912bd25b18eedfdab9c5a927cffe60d66f279f
umaru <15875339926@139.com>
2023-03-23 11:41:26 +0800

    添加异常堆栈

--- graylog2-server/src/main/java/org/etherfurnace/inputs/jsoninput/JSONCodec.java
+++ graylog2-server/src/main/java/org/etherfurnace/inputs/jsoninput/JSONCodec.java
@@ -17,6 +17,8 @@ import org.graylog2.plugin.inputs.codecs.AbstractCodec;
 import org.graylog2.plugin.inputs.codecs.CodecAggregator;
 import org.graylog2.plugin.inputs.transports.NettyTransport;
 import org.graylog2.plugin.journal.RawMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -24,9 +26,6 @@ import java.math.BigDecimal;
 import java.net.InetAddress;
 import java.util.LinkedHashMap;
 import java.util.UUID;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Codec(name = "json", displayName = "JSON 解码器")
 public class JSONCodec extends AbstractCodec {
@@ -45,8 +44,8 @@ public class JSONCodec extends AbstractCodec {
         final InetAddress remoteAddress = rawRemoteAddress == null ? null : rawRemoteAddress.getAddress();
         try {
             LinkedHashMap msg = objectMapper.readValue(new String(raw.getPayload(), Charsets.UTF_8),
-                new TypeReference<LinkedHashMap<String, Object>>() {
-                });
+                    new TypeReference<LinkedHashMap<String, Object>>() {
+                    });
             msg.forEach((key, value) -> {
                 if (value instanceof BigDecimal) {
                     msg.put(key, ((BigDecimal) value).doubleValue());
@@ -76,6 +75,7 @@ public class JSONCodec extends AbstractCodec {
             return message;
         } catch (Exception e) {
             LOG.error(e.getMessage(), e);
+            e.printStackTrace();
             return new Message(new String(raw.getPayload(), Charsets.UTF_8), null, raw.getTimestamp());
         }
 
